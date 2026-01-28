b74ac4beb341d56751de5618e91c95c5496542b0
Victor Mokut <vikmokut@gmail.com>
2026-01-28 22:13:46 +0100

    Document clear_inventory function
    Add docstring to clear_inventory function

---
+++ python/0x04_clear_list.py
@@ -0,0 +1,38 @@
+def clear_inventory():
+    """Clear a list and output each step
+
+    Args: None
+
+    Returns: No return Value
+    """
+    inventory = [
+        "Healing Potion",
+        "Iron Bar",
+        "Kite Shield",
+        "Shortsword",
+        "Leather Scraps",
+        "Tattered Cloth",
+    ]
+
+    print(f"inventory: {inventory}")
+
+    # don't touch above this line
+
+    for i in range(0, len(inventory)):
+        item = inventory.pop()
+
+        # don't touch below this line
+        print(f"Selling: {item}")
+        print(f"inventory: {inventory}")
+
+
+def test():
+    clear_inventory()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
